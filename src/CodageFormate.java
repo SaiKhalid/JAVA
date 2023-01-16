0f282254404849de6e7c8e78695521bcdfc4f1ec
Khalid Hicham Sai <khalid.sai@labom2iformation.fr>
2023-01-16 14:06:55 +0100

    Fundamentals

--- src/CodageFormate.java
+++ src/CodageFormate.java
@@ -1,4 +1,4 @@
-public class CodageFormaté {
+public class CodageFormate {
     public static void main(String[] args) {
         String firstName = "Khalid Hicham";
         String lastName = "Sai";
