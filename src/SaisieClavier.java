73e8fb6d646f011c89e032d200bbe83de0cebfc9
Khalid Hicham Sai <khalid.sai@labom2iformation.fr>
2023-01-16 11:58:40 +0100

    Fundamentals

--- src/SaisieClavier.java
+++ src/SaisieClavier.java
@@ -5,7 +5,18 @@ public class SaisieClavier {
         Scanner keyboardInput = new Scanner(System.in);
         System.out.println("Votre nom :");
         //String nom = keyboardInput.next(); prends juste un mot
+        //par example, si tu met : "Sai Khalid", il va prendre que "Sai".
         String nom = keyboardInput.nextLine(); //prend tout le text introduit
-        System.out.println(nom);
+        //System.out.println(nom);
+
+        System.out.println("Votre age :");
+        int age = keyboardInput.nextInt(); //ici, il prend que les entiers
+        //si par example on met "26 ans" il prend que 26
+
+
+        System.out.println("Votre poids (avec virgule, non pas un point) :");
+        double poids = keyboardInput.nextDouble(); //ici, il prend que les double
+        //si par example on met "89,9 kg" il prend que 89,9
+        System.out.printf("Je suis %s, j'ai %d ans et je pèse %.2f ", nom,age, poids);
     }
 }
