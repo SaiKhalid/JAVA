379b22237bd45204605a867eb24d5624e0a3023c
Khalid Hicham Sai <khalid.sai@labom2iformation.fr>
2023-01-16 12:02:06 +0100

    Fundamentals

---
+++ src/MathFunctions.java
@@ -0,0 +1,8 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+        double a = 7;
+        System.out.println(Math.pow(a,2));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.PI);
+    }
+}
