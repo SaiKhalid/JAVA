ccb2b3fc16a7cb43d4cb7bc9db67b3d85be1c12f
Khalid Hicham Sai <khalid.sai@labom2iformation.fr>
2023-01-16 11:31:32 +0100

    Distance/time

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double distance = 200;
+        double temps = 20;
+        double vitesse = distance/temps;
+        System.out.println(vitesse);
+    }
+}
